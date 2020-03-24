f5ac22db49cf1cca0a1fe5e39b2770ce90ff11f8
Josh Latour <jlatour88@gmail.com>
2020-03-24 13:07:38 -0500

    Added Submit btn with placeholder alert

--- screens/PullDetails/PullDetails.js
+++ screens/PullDetails/PullDetails.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { SafeAreaView, FlatList, StyleSheet } from 'react-native';
+import { SafeAreaView, Button, FlatList, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import ListViewItem from '../../components/ListViewItem/ListViewItem';
 import PullData from '../../TestData/PullDetails.example';
 
 export default function App() {
   const [dataArr, setDataArr] = useState(PullData);
+
+  const isFinished = dataArr.every((item => item.picked !== false));
 
   const handleToggleItem = itemId => {
     const newArr = dataArr.map(el =>
@@ -23,6 +25,11 @@ export default function App() {
             toggleItem={handleToggleItem}
           />}
         keyExtractor={(item, index) => index.toString()}
+      />
+      <Button
+        title='Submit'
+        disabled={!isFinished}
+        onPress={() => alert('Pullsheet marked as Pulled')}
       />
     </SafeAreaView>
   );
