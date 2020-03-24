9e1eaae28c229dc303d14761d8e6ce742faa6a68
Josh Latour <jlatour88@gmail.com>
2020-03-24 11:45:01 -0500

    Move picked items to bottom of list

--- screens/PullDetails/PullDetails.js
+++ screens/PullDetails/PullDetails.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text } from 'react-native';
+import { SafeAreaView, FlatList, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import ListViewItem from '../../components/ListViewItem/ListViewItem';
 import PullData from '../../TestData/PullDetails.example';
@@ -9,14 +9,14 @@ export default function App() {
 
   const handleToggleItem = itemId => {
     const newArr = dataArr.map(el =>
-      (el.id === itemId ? Object.assign({}, el, { 'checked': true }) : el))
+      (el.id === itemId ? Object.assign({}, el, { 'picked': !el.picked }) : el))
     setDataArr(newArr)
   }
 
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={dataArr}
+        data={dataArr.sort((a, b) => a.picked - b.picked)}
         renderItem={({ item }) =>
           <ListViewItem
             data={item}
