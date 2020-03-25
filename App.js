ca23372dd1abc324ea18543c16b33b308906ba68
Josh Latour <jlatour88@gmail.com>
2020-03-25 10:41:33 -0500

    Added statusbar constraints to styles

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import PullDetails from './screens/PullDetails/PullDetails';
+import Constants from 'expo-constants';
+import PullDetails from './screens/PullDetails';
 
 export default function App() {
   return (
@@ -14,7 +15,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
+    marginTop: Constants.statusBarHeight
+  }
 });
