296d68f0a4cfb2a4f350097f4b70ccb95c054587
Josh Latour <jlatour88@gmail.com>
2020-03-25 10:45:54 -0500

    Changed to Arrow function

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 import PullDetails from './screens/PullDetails';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
       <PullDetails />
     </View>
   );
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
