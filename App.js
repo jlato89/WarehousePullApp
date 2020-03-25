edc8cf5c0189d6eed3da78b38fcea26304bbd672
Josh Latour <jlatour88@gmail.com>
2020-03-25 11:10:48 -0500

    installed React-Navigation and added some placeholder screens

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Constants from 'expo-constants';
+import Home from './screens/Home';
+import Login from './screens/Login';
 import PullDetails from './screens/PullDetails';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <PullDetails />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="PullDetails" component={PullDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <PullDetails />
+    // </View>
   );
 }
 
