9ca049b34a4644407c0079f3a181ca60abadf9a1
Josh Latour <jlatour88@gmail.com>
2020-03-26 03:19:55 -0500

    Map thru MenuBtns to pass to MenuBtn component

--- screens/Home.js
+++ screens/Home.js
@@ -2,14 +2,46 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MenuBtn from '../components/MenuBtn/MenuBtn';
 
-const Home = () => {
+const MenuBtns = [
+  {
+    name: 'Pull List',
+    route: 'PullList',
+    imgUrl: 'http://simpleicon.com/wp-content/uploads/note-128x128.png'
+  },
+  {
+    name: 'Scheduler',
+    route: 'PullList',
+    imgUrl: 'http://simpleicon.com/wp-content/uploads/note-128x128.png'
+  },
+  {
+    name: 'Other Option',
+    route: 'PullList',
+    imgUrl: 'http://simpleicon.com/wp-content/uploads/note-128x128.png'
+  },
+]
+
+const Home = ({ navigation }) => {
   return (
-    <View>
-      <MenuBtn />
+    <View style={styles.container}>
+      {MenuBtns.map((btn, i) => (
+        <MenuBtn
+          key={i}
+          name={btn.name}
+          route={btn.route}
+          imgUrl={btn.imgUrl}
+          navigation={navigation}
+        />
+      ))}
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    margin: 10
+  }
+})
