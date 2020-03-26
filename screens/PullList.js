159a5466f06a69ae01ada88686f02aebaf44f6fe
jlatour88 <jlatour88@gmail.com>
2020-03-26 13:35:41 -0500

    Created basic PullList Structure

--- screens/PullList.js
+++ screens/PullList.js
@@ -1,14 +1,57 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react';
+import { StyleSheet, SafeAreaView, FlatList, Text, View, TouchableOpacity } from 'react-native';
+import PullListData from '../TestData/PullListData.example';
+
+function PullListItem({ listItem }) {
+  return (
+    <TouchableOpacity style={styles.listContainer} onPress={() => alert('pressed')}>
+      <View style={styles.listHeader}>
+        <Text style={styles.listText}>{listItem.orderNum}</Text>
+        <Text style={styles.listText}>{listItem.placedOn}</Text>
+      </View>
+      <View style={styles.listContent}>
+        <Text style={styles.listText}>{listItem.jobName}</Text>
+        <Text style={styles.listText}>{listItem.status}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
 
 const PullList = () => {
+  const [pullArr, setPullArr] = useState(PullListData);
+
   return (
-    <View>
-      <Text>Pull List</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        // data={dataArr.sort((a, b) => a.picked - b.picked)}
+        data={pullArr}
+        renderItem={({ item }) => <PullListItem listItem={item} />}
+        keyExtractor={item => item.id.toString()}
+      />
+    </SafeAreaView>)
 }
 
 export default PullList
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 5
+  },
+  listContainer: {
+    borderWidth: 2,
+    marginVertical: 5
+  },
+  listHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  listContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  listText: {
+    marginHorizontal: 5,
+    fontSize: 18
+  }
+});
