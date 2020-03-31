d84f760432f757095f1de241fc1a0b2541bc149b
jlatour88 <jlatour88@gmail.com>
2020-03-31 13:11:02 -0500

    Added header(orderNum, custName) and a notes section

--- screens/PullDetails.js
+++ screens/PullDetails.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { SafeAreaView, Button, FlatList, StyleSheet } from 'react-native';
+import { StyleSheet, SafeAreaView, View, Text, FlatList, Button, } from 'react-native';
 import ListViewItem from '../components/ListViewItem/ListViewItem';
 
 const PullDetails = ({ navigation, route }) => {
-  const { pullList } = route.params;
+  const { orderNum, custName, notes, pullList } = route.params.order;
   const [dataArr, setDataArr] = useState(pullList);
 
   const isFinished = dataArr.every((item => item.picked !== false));
@@ -16,16 +16,26 @@ const PullDetails = ({ navigation, route }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList
-        data={dataArr.sort((a, b) => a.picked - b.picked)}
-        renderItem={({ item }) =>
-          <ListViewItem
-            data={item}
-            toggleItem={handleToggleItem}
-          />}
-        keyExtractor={(item, index) => index.toString()}
-      />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>{orderNum}</Text>
+        <Text style={styles.headerText}>{custName}</Text>
+      </View>
+      <View style={styles.listItems}>
+        <FlatList
+          data={dataArr.sort((a, b) => a.picked - b.picked)}
+          renderItem={({ item }) =>
+            <ListViewItem
+              data={item}
+              toggleItem={handleToggleItem}
+            />}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+      <View style={styles.comments}>
+        <Text style={styles.commentsText}>{notes ? notes : 'There are currently no Notes'}</Text>
+      </View>
       <Button
+        style={styles.submitBtn}
         title='Submit'
         disabled={!isFinished}
         onPress={() => alert('Pullsheet marked as Pulled')}
@@ -40,4 +50,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+  },
+  headerText: {
+    fontSize: 16,
+  },
+  listItems: {
+    flex: 15,
+  },
+  comments: {
+    flex: 1,
+    paddingHorizontal: 5,
+  },
+  commentsText: {
+    fontSize: 16,
+  },
+  submitBtn: {
+    flex: 1,
+  }
 });
