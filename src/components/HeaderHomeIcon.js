aafc5d9c78fb552028e112fe3287cb388fcc0eb0
jlatour88 <jlatour88@gmail.com>
2020-03-31 20:34:30 -0500

    Changed form Image to Icon for homeBtn, removed old home-icon.png

--- src/components/HeaderHomeIcon.js
+++ src/components/HeaderHomeIcon.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { TouchableOpacity, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Icon } from 'react-native-elements';
 
 const HeaderHomeIcon = () => {
   const navigation = useNavigation();
   return (
-    <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-      <Image
-        style={{ width: 36, height: 36, tintColor: '#fff' }}
-        source={require('../assets/home-icon.png')}
-      />
-    </TouchableOpacity>
+    <Icon
+      name='home'
+      type='material'
+      color='#fff'
+      size={36}
+      onPress={() => navigation.navigate('Home')}
+    />
   )
 }
 
