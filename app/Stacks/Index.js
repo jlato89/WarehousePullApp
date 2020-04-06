604ef6dcbaf5cf057d8ea02c9f52dec5addf6cc6
jlatour88 <jlatour88@gmail.com>
2020-04-05 22:20:23 -0500

    Removed MainStack export due to  require cycles dub

--- app/Stacks/Index.js
+++ app/Stacks/Index.js
@@ -1,4 +1,3 @@
-export { default as MainStack } from './MainStack';
 export { default as OrdersStack } from './OrdersStack';
 export { default as UserStack } from './UserStack';
 export { default as SchedulerStack } from './SchedulerStack';
