0de6f5b64e1aaee2c2ced282f23ac3732666674b
jlatour88 <jlatour88@gmail.com>
2020-04-13 12:28:28 -0500

    gridFormat for HomeScreen

---
+++ app/components/gridFormat.js
@@ -0,0 +1,12 @@
+function formatData(menuOptions, numColumns) {
+  const totalRows = Math.floor(menuOptions.length / numColumns);
+  let totalLastRow = menuOptions.length - (totalRows * numColumns);
+
+  while (totalLastRow !== 0 && totalLastRow !== numColumns) {
+    menuOptions.push({ empty: true })
+    totalLastRow++
+  }
+  return menuOptions
+}
+
+export default formatData
