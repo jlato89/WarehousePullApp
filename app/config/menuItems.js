29cdabb892bc5edef23cf6f0005f7569349dfe5f
jlatour88 <jlatour88@gmail.com>
2020-04-13 12:30:16 -0500

    menuOptions for homescreen

---
+++ app/config/menuItems.js
@@ -0,0 +1,19 @@
+const menuItems = [
+  {
+    name: 'Current Orders',
+    iconName: 'assignment',
+    route: 'OrderStack'
+  },
+  {
+    name: 'Scheduler',
+    iconName: 'date-range',
+    route: 'SchedulerStack'
+  },
+  {
+    name: 'User Management',
+    iconName: 'supervisor-account',
+    route: 'UserStack'
+  },
+]
+
+export default menuItems
