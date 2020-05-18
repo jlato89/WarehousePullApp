bb99ed186cd40676fe8b91cf1d931e71fa36bb55
Josh Latour <jlatour88@gmail.com>
2020-05-18 16:31:13 -0500

    Started adding more pullInfo to the header

--- app/components/PullDetails/PullInfo.js
+++ app/components/PullDetails/PullInfo.js
@@ -4,8 +4,15 @@ import { StyleSheet, View, Text } from 'react-native';
 const PullInfo = ({ quote, customer }) => {
   return (
     <View style={styles.wrapper}>
-      <Text style={styles.headerText}>{quote}</Text>
-      <Text style={styles.headerText}>{customer}</Text>
+      <View style={styles.topHeader}>
+        <Text style={styles.headerText}>#{quote}</Text>
+        <Text style={styles.headerText}>Date and Time</Text>
+      </View>
+      <View style={styles.bottomHeader}>
+        <Text style={styles.custName}>{customer}</Text>
+        <Text style={styles.pullType}>Pull Type</Text>
+        <Text style={styles.percent}>Percent</Text>
+      </View>
     </View>
   );
 };
@@ -14,14 +21,32 @@ export default PullInfo;
 
 const styles = StyleSheet.create({
   wrapper: {
+    flex: 1.2,
+    paddingHorizontal: 10,
+    backgroundColor: '#b7b7b7',
+  },
+  topHeader: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingHorizontal: 10,
-    backgroundColor: '#bdb7b7',
   },
-  headerText: {
-    fontSize: 16,
+  bottomHeader: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  custName: {
+    flex: 2,
+    textAlign: 'center',
+  },
+  pullType: {
+    flex: 1,
+    textAlign: 'right',
+  },
+  percent: {
+    flex: 1,
+    textAlign: 'right',
   },
 });
